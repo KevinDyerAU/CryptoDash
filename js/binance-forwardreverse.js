1662ab36706cf2cd0cfea54e1eeddb8dde65efa0
Kevin <kdyer1000@gmail.com>
2020-10-23 15:42:40 +1000

    Updated symbols

--- js/binance-forwardreverse.js
+++ js/binance-forwardreverse.js
@@ -16,23 +16,23 @@ let sleep = (ms) => new Promise (resolve => setTimeout (resolve, ms))
  * @param {*} ask 
  * @param {*} tradeValue 
  */
-const calculateTradeValue = function(ask, tradeValue,  precision = undefined) {
+const calculateBuyValue = function(ask, tradeValue,  precision = undefined) {
     ask = ccxt.decimalToPrecision (ask, ccxt.ROUND, precision, ccxt.TICK_SIZE)
     parseFloat (ask)
 
-    log('asking price: '.yellow, ask)
+    // log('I can buy for : '.yellow, ask)
 
-    log('amount to be sold: '.yellow, tradeValue)
+    // log('amount to be sold: '.yellow, tradeValue)
 
     let value = (tradeValue/ask)
-    log('tradeValue before fee: '.yellow, value)
+    // log('tradeValue before fee: '.yellow, value)
 
     let fee = value*0.001
-    log('fee: '.red, fee)
+    // log('fee: '.red, fee)
 
     value = value - fee
 
-    log('tradeValue after fee: '.green, value)
+    // log('tradeValue after fee: '.green, value)
 
     return value
 }
@@ -41,71 +41,50 @@ const calculateTradeValue = function(ask, tradeValue,  precision = undefined) {
  * Calculate total based on asking prices minus maker fee.
  * Fee = 0.1% so  multiply by 0.001 and subtract from the tradeValue
  * 
- * @param {*} ask 
+ * @param {*} bid 
  * @param {*} tradeValue 
  */
-const calculateReverseTradeValue = function(ask, tradeValue,  precision = undefined) {
-    ask = ccxt.decimalToPrecision (ask, ccxt.ROUND, precision, ccxt.TICK_SIZE)
-    parseFloat (ask)
+const calculateSellValue = function(bid, tradeValue,  precision = undefined) {
+    bid = ccxt.decimalToPrecision (bid, ccxt.ROUND, precision, ccxt.TICK_SIZE)
+    parseFloat (bid)
 
-    log('asking price: '.yellow, ask)
+    // log('I can sell for price: '.yellow, bid)
 
-    log('amount to be sold: '.yellow, tradeValue)
+    // log('amount to be sold: '.yellow, tradeValue)
 
-    let value = (tradeValue*ask)
-    log('tradeValue before fee: '.yellow, value)
+    let value = (tradeValue*bid)
+    // log('tradeValue before fee: '.yellow, value)
 
     let fee = value*0.001
-    log('fee: '.red, fee)
+    // log('fee: '.red, fee)
 
     value = value - fee
 
-    log('tradeValue after fee: '.green, value)
+    // log('tradeValue after fee: '.green, value)
 
     return value
 }
 
 /**
- * Calculate AUD based on bid price minus taker fee.
- * Fee = 0.1% so  multiply by 0.001 and subtract from the tradeValue
- * 
- * @param {*} bidAUDPrice 
- * @param {*} tradeValue 
- */
-const convertBackToAUD = function(bidAUDPrice,  tokenValue) {
-    let value = tokenValue*bidAUDPrice
-    log('AUD value before fee: '.yellow, value)
-
-    let fee = value*0.001
-    log('fee: '.red, fee)
-
-    value = value - fee
-
-    log('AUD after fee: '.green, value)
-
-    return value
-}
-
-/**
- * Check to see if we should execute trade after fees based on $1000 AUD
+ * Check to see if we should execute trade after fees based on $1000 BASE
  * @param {*} baseTrade 
  * @param {*} interTrade 
  * @param {*} finalTrade 
  * @param {*} precision 
  */
 const testTradeCycle = function (baseAsk, interAsk, finalAsk,  precision = undefined) {
-    log('------- Forward --------'.green)
+    // log('------- Forward --------'.green)
 
     let initValue = 1000
 
-    //step one AUD -> firstToken
-    let baseTokenValue = calculateTradeValue(baseAsk, initValue, precision)
+    //step one BASE -> firstToken
+    let baseTokenValue = calculateBuyValue(baseAsk, initValue, precision)
 
     //step two firstToken -> secondToken noting that intermediary pair will contain base
-    let intermediaryValue = calculateTradeValue(interAsk, baseTokenValue, precision)
+    let intermediaryValue = calculateSellValue(interAsk, baseTokenValue, precision)
 
-    //final trade intemdiary back to $AUD
-    let netResult = convertBackToAUD(finalAsk, intermediaryValue)
+    //final trade intemdiary back to $USDT
+    let netResult = calculateSellValue(finalAsk, intermediaryValue, precision)
 
     return netResult
 }
@@ -119,17 +98,17 @@ const testTradeCycle = function (baseAsk, interAsk, finalAsk,  precision = undef
  * @param {*} precision 
  */
 const testReverseTradeCycle = function (baseAsk, interAsk, finalAsk,  precision = undefined) {
-    log('------- Reverse --------'.green)
+    // log('------- Reverse --------'.green)
     let initValue = 1000
 
-    //step one AUD -> firstToken
-    let baseTokenValue = calculateTradeValue(baseAsk, initValue, precision)
+    //step one BASE -> firstToken
+    let baseTokenValue = calculateBuyValue(baseAsk, initValue, precision)
 
     //step two firstToken -> secondToken noting that intermediary pair will contain base
-    let intermediaryValue = calculateReverseTradeValue(interAsk, baseTokenValue, precision)
+    let intermediaryValue = calculateBuyValue(interAsk, baseTokenValue, precision)
 
     //final trade intemdiary back to $AUD
-    let netResult = convertBackToAUD(finalAsk, intermediaryValue)
+    let netResult = calculateSellValue(finalAsk, intermediaryValue, precision)
 
     return netResult
 }
@@ -143,40 +122,216 @@ const testReverseTradeCycle = function (baseAsk, interAsk, finalAsk,  precision 
     })
 
     try {
+            let pairs = ['AAVE',
+            'ADA',
+            'AION',
+            'ALGO',
+            'ALPHA',
+            'ANKR',
+            'ANT',
+            'ARDR',
+            'ATOM',
+            'AVAX',
+            'BAL',
+            'BAND',
+            'BAT',
+            'BCC',
+            'BCH',
+            'BEAM',
+            'BEL',
+            'BLZ',
+            'BNB',
+            'BNT',
+            'BSV',
+            'BTS',
+            'BTT',
+            'BZRX',
+            'CELR',
+            'CHR',
+            'CHZ',
+            'COCOS',
+            'COMP',
+            'COS',
+            'COTI',
+            'CRV',
+            'CTSI',
+            'CTXC',
+            'CVC',
+            'DAI',
+            'DASH',
+            'DATA',
+            'DCR',
+            'DENT',
+            'DGB',
+            'DIA',
+            'DOCK',
+            'DOGE',
+            'DOT',
+            'DREP',
+            'DUSK',
+            'EGLD',
+            'ENJ',
+            'EOS',
+            'ERD',
+            'ETC',
+            'ETH',
+            'FET',
+            'FIL',
+            'FIO',
+            'FLM',
+            'FTM',
+            'FTT',
+            'FUN',
+            'GTO',
+            'GXS',
+            'HBAR',
+            'HC',
+            'HIVE',
+            'HNT',
+            'HOT',
+            'ICX',
+            'INJ',
+            'IOST',
+            'IOTA',
+            'IOTX',
+            'IRIS',
+            'JST',
+            'KAVA',
+            'KEY',
+            'KMD',
+            'KNC',
+            'KSM',
+            'LEND',
+            'LINK',
+            'LRC',
+            'LSK',
+            'LTC',
+            'LTO',
+            'LUNA',
+            'MANA',
+            'MATIC',
+            'MBL',
+            'MCO',
+            'MDT',
+            'MFT',
+            'MITH',
+            'MKR',
+            'MTL',
+            'NANO',
+            'NBS',
+            'NEAR',
+            'NEO',
+            'NKN',
+            'NMR',
+            'NPXS',
+            'NULS',
+            'OCEAN',
+            'OGN',
+            'OMG',
+            'ONE',
+            'ONG',
+            'ONT',
+            'ORN',
+            'OXT',
+            'PAX',
+            'PAXG',
+            'PERL',
+            'PNT',
+            'QTUM',
+            'REN',
+            'REP',
+            'RLC',
+            'RSR',
+            'RUNE',
+            'RVN',
+            'SAND',
+            'SC',
+            'SNX',
+            'SOL',
+            'SRM',
+            'STMX',
+            'STORJ',
+            'STORM',
+            'STPT',
+            'STRAT',
+            'STX',
+            'SUN',
+            'SUSHI',
+            'SXP',
+            'TCT',
+            'TFUEL',
+            'THETA',
+            'TOMO',
+            'TRB',
+            'TROY',
+            'TRX',
+            'TUSD',
+            'UMA',
+            'UNI',
+            'UTK',
+            'VEN',
+            'VET',
+            'VITE',
+            'WAN',
+            'WAVES',
+            'WIN',
+            'WING',
+            'WNXM',
+            'WRX',
+            'WTC',
+            'XLM',
+            'XMR',
+            'XRP',
+            'XTZ',
+            'XVS',
+            'XZC',
+            'YFI',
+            'YFII',
+            'ZEC',
+            'ZEN',
+            'ZIL',
+            'ZRX']
 
-            // fetch tickers
-            let forwardTicker = await exchange.fetchTicker ('ETH/AUD')
-            console.log ('ETH/AUD', 'dateTime', forwardTicker['datetime'], 'bid', forwardTicker['bid'], 'ask', forwardTicker['ask'])
+            let primaryMarket = "USDT"
+            let intermediaryMarket = "BTC"
 
-            let intermediaryTicker = await exchange.fetchTicker ('BNB/ETH')
-            console.log ('BNB/ETH', 'dateTime', intermediaryTicker['datetime'], 'bid', intermediaryTicker['bid'], 'ask',intermediaryTicker['ask'])
+            for (let symbol of pairs) {
 
-            let reverseTicker = await exchange.fetchTicker ('BNB/AUD')
-            console.log ('BNB/AUD', 'dateTime', reverseTicker['datetime'], 'bid', reverseTicker['bid'],  'ask',reverseTicker['ask'])
+                // fetch tickers
+                let forwardTicker = await exchange.fetchTicker (symbol+'/'+primaryMarket)
+                // console.log (symbol+'/'+primaryMarket, 'dateTime', forwardTicker['datetime'], 'bid', forwardTicker['bid'], 'ask', forwardTicker['ask'])
+
+                let intermediaryTicker = await exchange.fetchTicker (symbol+'/'+intermediaryMarket)
+                // console.log (symbol+'/'+intermediaryMarket, 'dateTime', intermediaryTicker['datetime'], 'bid', intermediaryTicker['bid'], 'ask',intermediaryTicker['ask'])
+
+                let reverseTicker = await exchange.fetchTicker (intermediaryMarket+'/'+primaryMarket)
+                // console.log (intermediaryMarket+'/'+primaryMarket, 'dateTime', reverseTicker['datetime'], 'bid', reverseTicker['bid'],  'ask',reverseTicker['ask'])
 
 
+                //TEST TRADE CYCLE INCL FEES
 
-
-            // Test forward cycle on $1000AUD
-            let result = testTradeCycle(forwardTicker['ask'], intermediaryTicker['ask'], reverseTicker['bid'],0.000000001)
-            if(result < 1000) {
-                //Test reverse cycle on $1000AUD
-                result = testReverseTradeCycle(reverseTicker['ask'], intermediaryTicker['ask'], forwardTicker['bid'],0.000000001)
+                // Test forward cycle on $1000AUD
+                let result = testTradeCycle(forwardTicker['ask'], intermediaryTicker['ask'], reverseTicker['bid'],0.000000001)
                 if(result < 1000) {
-                    console.log ('NO REVERSE TRADE')
+                    //Test reverse cycle on $1000AUD
+                    result = testReverseTradeCycle(reverseTicker['ask'], intermediaryTicker['ask'], forwardTicker['bid'],0.000000001)
+                    if(result < 1000) {
+                        // log (symbol+' NO TRADE'.red)
+                    }
+                    else {
+                        log (symbol+' Reverse TRADE Success!!'.green)
+                        log (result)
+                    }
+
                 }
                 else {
-                    console.log ('Reverse TRADE Success')
+                    log (symbol+' Forward TRADE Success!!'.green)
+                    log (result)
                 }
-
             }
-            else {
-                console.log ('Forward TRADE Success')
-            }
-
             
 
-            //Sample ONLY
+            //Execute TRADE Sample ONLY
 
             // // create market order
             // let marketOrder = await exchange.createOrder('ETH/USD', 'market', 'buy', 0.01)
